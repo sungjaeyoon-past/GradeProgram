c1a7c025e6a378600fa89cd166bf4afa78c46260
kmlee95 <kmlee95@naver.com>
2018-11-07 03:03:23 +0900

    메뉴바 조정

--- src/Frame/SuperFrame.java
+++ src/Frame/SuperFrame.java
@@ -1,15 +1,33 @@
 package Frame;
 
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.*;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import Main.GradeP;
 
 public class SuperFrame extends JFrame{
+	ImageIcon icon;
+	JScrollPane scrollPane;
+	
 	public SuperFrame() {
-		Container c = this.getContentPane();		
-		JPanel p =new JPanel();
-		p.setLayout(new FlowLayout(FlowLayout.LEFT));
+		//Container c = this.getContentPane();		
+		//JPanel p = new JPanel();
+		//p.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		icon = new ImageIcon("../images/mainImage.PNG");
+		JPanel p1 = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+		scrollPane = new JScrollPane(p1);
+		setContentPane(scrollPane);
+
 		
 		this.setJMenuBar(new MenuBar());
 		this.setTitle("己利 包府 橇肺弊伐");
