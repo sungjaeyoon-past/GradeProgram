faac5bd09bf269d6cb07666f035811ee8c017b05
codingHack <dbstjdwo1000@naver.com>
2018-11-22 18:45:50 +0900

    merge

--- src/Frame/AttendPanel.java
+++ src/Frame/AttendPanel.java
@@ -5,12 +5,19 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class AttendPanel extends TopPanel{
 	public AttendPanel() {
 		setBackground(Color.WHITE);
 		setLayout(null);
+		
+		creatComponentPanel();
+		
+	}
+	
+	public void creatComponentPanel() {
 		String[] str = {"학번","이름"};
 		
 		JComboBox combo = new JComboBox(str);
@@ -25,9 +32,9 @@ public class AttendPanel extends TopPanel{
 		jb.setFont(new Font("a개미야", Font.BOLD, 25));
 		jb.setBounds(310, 40, 90, 40);
 		
-		
 		add(combo);
 		add(jt);
-		add(jb);
+		add(jb);	
 	}
+	
 }
